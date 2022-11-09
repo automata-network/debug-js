b9b1d85ca06a71279d5eec57d36f3d68613b2841
duxiaofeng-github <csdxf297951292@gmail.com>
2022-11-09 20:07:58 +0800

    update example

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,7 +4,7 @@ const debugJs = new DebugJs({
   appName: "example",
   collectorUrl: "http://localhost:9000/collect",
   logLevel: LogLevel.Info,
-  requestInterval: 2000,
+  requestInterval: 5000,
 });
 
 debugJs.setUid("johnny");
